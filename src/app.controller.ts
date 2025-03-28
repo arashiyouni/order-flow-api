e54ec5a3b29182b8d387cc52598f085391dd5eb3
arashiyouni <angie.espinoza.campos@gmail.com>
2025-03-27 23:21:53 -0600

    ✨ added hello controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { httpResponse } from './common/interface/https-commons.interface';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    const fn = async () => {
+      return this.appService.getHello()
+    }
+    return await httpResponse(fn);
   }
 }
