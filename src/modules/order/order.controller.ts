13263f7e9710bbf0501ca60731832bc0fca6762f
arashiyouni <angie.espinoza.campos@gmail.com>
2025-03-29 20:31:33 -0600

    ✨ added endpoint edit order

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Post, Body, UseGuards, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Param, Patch, Delete } from '@nestjs/common';
 import { OrderService } from './order.service';
-import { CreateOrderDto } from './dto/create-order.dto';
+import { CreateOrderDto, CreateOrderPerUserDto } from './dto/create-order.dto';
 import { RolesGuard } from '../auth/guard/roles.guard';
 import { AuthGuard } from '../auth/guard/auth.guard';
 import { httpResponse } from 'src/common/interface/https-commons.interface';
@@ -14,10 +14,10 @@ export class OrderController {
   constructor(private readonly orderService: OrderService) { }
 
   @Roles(ROLE.USER)
-  @Get(':orderId')
+  @Delete(':id')
   async GetOrder(@Param() orderId: OrderDto) {
     const fn = async () => {
-      return await this.orderService.deleteOrder(orderId)
+      return await this.orderService.deleteOrder(orderId.id)
     }
     return await httpResponse(fn)
   }
@@ -27,6 +27,18 @@ export class OrderController {
   async createOrder(@Body() createOrderDto: CreateOrderDto) {
     const fn = async () => {
       return this.orderService.create(createOrderDto)
+    }
+    return await httpResponse(fn)
+  }
+
+  @Patch('/item/:id')
+  @Roles(ROLE.USER)
+  async editOrder(
+    @Param() orderId: OrderDto,
+    @Body() editOrderDto: CreateOrderPerUserDto
+  ) {
+    const fn = async () => {
+      return this.orderService.editOrder(orderId.id, editOrderDto)
     }
     return await httpResponse(fn)
   }
